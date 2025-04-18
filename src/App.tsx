52cf8016a3f4cffab37c973e9ca593ad557eb00e
Daniel Chung <daniel20020228@gmail.com>
2025-04-18 13:54:52 +1000

    Create Board button on top of TopBtn in App.tsx that links to Board.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { useState } from "react";
 import Footer from "./components/Footer";
 import Intro from "./components/Intro";
@@ -19,11 +19,16 @@ const TopBtn = styled.button`
     color: white;
     border: 3px solid white;
     position: fixed;
-
+    text-align: center;
+    padding-top: 7px;
     &:hover {
         background-color: white;
         color: black;
     }
+`;
+
+const BoardBtn = styled(TopBtn)`
+    bottom: 80px;
 `;
 
 function App() {
@@ -75,6 +80,24 @@ function App() {
                     />
                 </svg>
             </TopBtn>
+            <BoardBtn>
+                <Link to='/board'>
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth={1.5}
+                        stroke='currentColor'
+                        className='size-6'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z'
+                        />
+                    </svg>
+                </Link>
+            </BoardBtn>
         </Router>
     );
 }
